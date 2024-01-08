780e1ef5b2989f87ccea14a7f602bf15cf6640f4
友光 草場 <tomokusaba@tomokusaba.onmicrosoft.com>
2024-01-08 15:26:58 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+IConfigurationRoot config = new ConfigurationBuilder()
+    .AddEnvironmentVariables()
+    .AddUserSecrets<Program>()
+    .Build();
+
+
+string deploymentName = config["OpenAI:DeploymentName"] ?? throw new InvalidOperationException("OpenAI:DeploymentName is not set.");
+string modelId = config["OpenAI:ModelId"] ?? throw new InvalidOperationException("OpenAI:ModelId is not set.");
+string endpoint = config["OpenAI:Endpoint"] ?? throw new InvalidOperationException("OpenAI:BaseUrl is not set.");
+string key = config["OpenAI:Key"] ?? throw new InvalidOperationException("OpenAI:Key is not set.");
+
+Kernel kernel = Kernel.CreateBuilder()
+ .AddAzureOpenAIChatCompletion(
+    deploymentName,
+    endpoint,
+    key).Build();
+
+var chat = kernel.CreateFunctionFromPrompt(
+@"MM-dd-yyyy を入力として受け取って yyyy/MM/dd を出力する。
+結果のみ出力してください。
+変換前：{{$message}}");
+
+ChatHistory history = [];
+while (true)
+{
+    Console.Write("User: ");
+    string? message = Console.ReadLine();
+    if (message == "exit")
+    {
+        break;
+    }
+    else
+    {
+        var result = await kernel.InvokeAsync(chat, new() {
+            { "message", message},
+        });
+        Console.WriteLine(result.ToString());
+        history.AddUserMessage(message!);
+        history.AddAssistantMessage(result.ToString());
+    }
+
+
+}
